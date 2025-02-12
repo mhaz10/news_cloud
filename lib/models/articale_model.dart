b8dcee48b9ef31a5e3359dd3088b13d4ebe7672d
mohamed <mohammedhosny53@gmail.com>
2025-02-12 04:03:29 +0200

    new project

--- lib/models/articale_model.dart
+++ lib/models/articale_model.dart
@@ -2,14 +2,16 @@ class ArticaleModel {
   final String? image;
   final String title;
   final String? subTitle;
+  final String aritcleUrl;
 
-  ArticaleModel({required this.image, required this.title, required this.subTitle});
+  ArticaleModel({required this.image, required this.title, required this.subTitle, required this.aritcleUrl});
 
   factory ArticaleModel.fromJson(Map<String, dynamic> json) {
     return ArticaleModel(
         image: json['urlToImage'] ?? '',
         title: json['title'],
         subTitle: json['description'] ?? '',
+        aritcleUrl: json['url']
     );
   }
 }
--- lib/widgets/news_tile.dart
+++ lib/widgets/news_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_cloud/models/articale_model.dart';
+import 'package:news_cloud/views/web_view.dart';
 
 
 class NewsTile extends StatelessWidget {
@@ -9,41 +10,46 @@ class NewsTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        ClipRRect(
-            borderRadius: BorderRadius.circular(6),
-            child: articleModel.image != '' ? Image.network(
-              articleModel.image!,
-              height: 200,
-              width: double.infinity,
-              fit: BoxFit.cover,
-              errorBuilder: (context, error, stackTrace) => Center(child: Icon(Icons.error)),
-            ) : Center(child: Icon(Icons.error,))
-        ),
-        const SizedBox(
-          height: 12,
-        ),
-        Text(
-          articleModel.title,
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-          style: const TextStyle(
-            color: Colors.black87,
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => WebView(url: articleModel.aritcleUrl)));
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ClipRRect(
+              borderRadius: BorderRadius.circular(6),
+              child: articleModel.image != '' ? Image.network(
+                articleModel.image!,
+                height: 200,
+                width: double.infinity,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) => Center(child: Icon(Icons.error)),
+              ) : Center(child: Icon(Icons.error,))
           ),
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        Text(
-          articleModel.subTitle!,
-          maxLines: 2,
-          style: const TextStyle(color: Colors.grey, fontSize: 14),
-        )
-      ],
+          const SizedBox(
+            height: 12,
+          ),
+          Text(
+            articleModel.title,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              color: Colors.black87,
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(
+            articleModel.subTitle!,
+            maxLines: 2,
+            style: const TextStyle(color: Colors.grey, fontSize: 14),
+          )
+        ],
+      ),
     );
   }
 }
