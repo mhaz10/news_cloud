4194441a5976e314dfd957995d479a52e738e44f
mohamed <mohammedhosny53@gmail.com>
2025-02-12 04:03:42 +0200

    new project

---
+++ lib/views/web_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebView extends StatefulWidget {
+  const WebView({super.key, required this.url});
+
+  final String url;
+
+  @override
+  State<WebView> createState() => _WebViewState();
+}
+
+class _WebViewState extends State<WebView> {
+
+  late WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.disabled)
+      ..loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: WebViewWidget(
+              controller: controller)
+      ),
+    );
+  }
+}
